ad82a092b3f03b82dd05cd97053b9f69724f443d
botaskybe1 <62637272+botaskybe1@users.noreply.github.com>
2020-09-24 19:09:24 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,42 @@
+import React, { Component, useState, useEffect } from 'react';
+import './App.css';
+import './Login.css';
+import './blox.css';
+import './converter.css';
+
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './components/main';
+import { Link } from 'react-router-dom';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="demo-big-content">
+        <Layout>
+          <Header className="header-color" title={<Link style={{ textDecoration: 'none', color: 'white' }} to="/">One Stock Data-service</Link>} scroll>
+            <Navigation>
+              <div class="form-group">
+                <input type="text" class="form-control" id="usr"></input>
+              </div>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+            </Navigation>
+          </Header>
+          <Drawer title={<Link style={{ textDecoration: 'none', color: 'black' }} to="/">Data-service</Link>}>
+            <Navigation>
+
+            </Navigation>
+          </Drawer>
+          <Content>
+            <div className="page-content" />
+            <Main />
+          </Content>
+        </Layout>
+      </div>
+
+    );
+  }
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import './blox.css';
+import registerServiceWorker from './registerServiceWorker';
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+ReactDOM.render(
+  <BrowserRouter>
+<App />
+  </BrowserRouter>
+  , document.getElementById('root'));
+registerServiceWorker();
